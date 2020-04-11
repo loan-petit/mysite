2b7bc65580f4006d8031b344821acf6f54ecf110
loan-petit <petit.loan1@gmail.com>
2020-04-11 17:46:11 +0200

    feature: Setup script to generate necessary secrets on remote Swarm.

--- swarm/build_and_push_images.sh
+++ swarm/build_and_push_images.sh
@@ -32,7 +32,7 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 # Build Docker image for Gatsby frontend using secrets stored in SECRETS_DIR.
 DOCKER_BUILDKIT=1 docker build --no-cache --progress=plain \
   --tag mysite:gatsby \
-  --build-arg GHOST_API_URL=http://loanpetit.com:8080 \
+  --build-arg GHOST_API_URL=http://www.loanpetit.com:8080 \
   --secret id=GHOST_CONTENT_API_KEY,src=$SECRETS_DIR/GHOST_CONTENT_API_KEY.txt \
   $SOURCE_DIR/../gatsby
 
@@ -40,3 +40,4 @@ DOCKER_BUILDKIT=1 docker build --no-cache --progress=plain \
 GATSBY_IMAGE_ID=$(docker image inspect mysite:gatsby --format='{{ .Id }}')
 docker tag $GATSBY_IMAGE_ID petitloan/mysite:gatsby
 docker push petitloan/mysite
+
--- swarm/init_docker_secrets.sh
+++ swarm/init_docker_secrets.sh
@@ -1,10 +1,61 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 
 if ! [ -x "$(command -v docker)" ]; then
   echo 'Error: docker is not installed.' >&2
   exit 1
 fi
 
-cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*_+-' | head -c 15 | docker secret create MYSQL_ROOT_PASSWORD -
-cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*_+-' | head -c 15 | docker secret create MYSQL_PASSWORD -
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -i|--identity-file)
+      IDENTITY_FILE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --remote-destination)
+      REMOTE_DESTINATION="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *)                   # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift              # past argument
+      ;;
+  esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+REMOTE_DOCKER_SOCK_PATH="./.remote-docker.sock"
+
+# Open SSH tunnel to remote Docker engine
+if [ ${REMOTE_DESTINATION+x} ]; then
+  ssh -o StrictHostKeyChecking=no \
+    -i $IDENTITY_FILE \
+    -f -M -S $REMOTE_DOCKER_SOCK_PATH \
+    -N -L localhost:2376:/var/run/docker.sock \
+    $REMOTE_DESTINATION
+      DOCKER_HOST_LIST="-H localhost:2376"
+fi
+
+# Generate secrets
+secrets=(
+  MYSQL_ROOT_PASSWORD
+  MYSQL_PASSWORD
+)
+for secret in "${secrets[@]}"; do
+  if [ -z "$(docker $DOCKER_HOST_LIST secret ls -f name=$secret -q)" ]; then
+    cat /dev/urandom | \
+      tr -dc '0-9a-zA-Z!@#$%^&*_+-' | \
+      head -c 15 | \
+      docker $DOCKER_HOST_LIST secret create $secret -
+  fi
+done
+
+# Close SSH tunnel
+if [ -n ${REMOTE_DESTINATION+x} ]; then
+  ssh -S $REMOTE_DOCKER_SOCK_PATH -O exit $REMOTE_DESTINATION
+fi
